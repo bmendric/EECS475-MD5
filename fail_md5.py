c29c495209c45888afa6433fa7a980d7d116d74f
Brandon Mendrick <bmendric@umich.edu>
2016-12-02 10:50:03 -0500

    MD5 implementation complete

---
+++ fail_md5.py
@@ -0,0 +1,209 @@
+# -*- coding: utf-8 -*-
+"""MD5 Implementation
+
+This is an implementation of the MD5 algorithm.
+This implementation is intended for academic use only.
+
+Authors:
+	Brandon Mendrick
+	Neil Orans
+
+"""
+
+from math import floor, sin
+
+class MD5:
+	"""MD5 class used to calculate MD5 hash values."""
+
+	# Number of 32 bit strings
+	bit32 = 2**32
+
+	# Number of 64 bit strings
+	bit64 = 2**64
+
+	# Amount of shift per round
+	shift = [
+		7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+		5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+		4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+		6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21
+	]
+
+	# Constants adding during each round
+	constant = [None] * 64
+	for i in range(0, 64, 1):
+		constant[i] = floor(bit32 * abs(sin(i + 1)))
+
+	def __init__(self, message):
+		# Initial word values
+		self.A = 0x67452301
+		self.B = 0xefcdab89
+		self.C = 0x98badcfe
+		self.D = 0x10325476
+
+		self.parse(message)
+
+		self.calculate()
+
+		self.output()
+
+		return
+
+	def _rotate(self, target, num):
+		"""Left rotate function defined in MD5."""
+		ynum = 32 - num
+		lhs = (target << num)
+		rhs = (target >> ynum)
+		tmp = (lhs | rhs) % MD5.bit32
+		# print(hex(lhs), hex(rhs), ynum, hex(tmp))
+		return tmp
+
+	def calculate(self):
+		"""Compute the A, B, C, D values for MD5."""
+		# For each 512-bit block
+		for j in range(1, self.numBlocks + 1):
+			currentBlock = self.parsedMessage[j]
+
+			currentA = self.A
+			currentB = self.B
+			currentC = self.C
+			currentD = self.D
+			function = None
+			position = None
+
+			# Main loop
+			for i in range(64):
+				# Setting the function and position variables for this round
+				if 0 <= i <= 15:
+					function = (currentB & currentC) | ((~currentB) & currentD)
+					position = i
+
+				elif 16 <= i <= 31:
+					function = (currentD & currentB) | ((~currentD) & currentC)
+					position = ((5 * i) + 1) % 16
+
+				elif 32 <= i <= 47:
+					function = currentB ^ currentC ^ currentD
+					position = ((3 * i) + 5) % 16
+
+				elif 48 <= i <= 63:
+					function = currentC ^ (currentB | (~currentD))
+					position = (7 * i) % 16
+
+				else:
+					print("Something went horribly wrong")
+					return
+
+				# Setting the A, B, C, D variables after the round
+				tempD = currentD
+				currentD = currentC
+				currentC = currentB
+
+				# Calculating B based on MD5 algorithm
+				rotateTarget = (currentA + function + MD5.constant[i] + currentBlock[position]) % MD5.bit32
+				print(hex(rotateTarget), hex(currentA), hex(function), hex(MD5.constant[i]), hex(currentBlock[position]))
+				currentB = (currentB + self._rotate(rotateTarget, MD5.shift[i])) % MD5.bit32
+				# print(hex(currentB))
+				
+				currentA = tempD
+
+				return
+
+			# Adding the results of the round to the overall A, B, C, D variables
+			self.A = (self.A + currentA) % MD5.bit32
+			self.B = (self.B + currentB) % MD5.bit32
+			self.C = (self.C + currentC) % MD5.bit32
+			self.D = (self.D + currentD) % MD5.bit32
+
+	def parse(self, message):
+		"""Parse message into 512-bit chunks and pad appropriately."""
+		# Message length in bits mod 2**64 for last 64-bits of padding
+		self.msgLen = (len(message.encode('utf-8')) * 8) % MD5.bit64
+
+		# Creating counter for number of blocks
+		self.numBlocks = 0
+
+		# Creating container for blocks
+		self.parsedMessage = {}
+
+		# Creating array of integers from the message
+		msgInt = [ord(x) for x in message]
+
+		# Counter for the number of bits processed
+		bitsProcessed = 0
+
+		currentBlock = 0b0
+		while len(msgInt) > 0:		#FIXME should support empty messages. Not sure what happens....
+			currentCharacter = msgInt.pop(0)
+
+			# Appending the character to the current block
+			currentBlock = currentBlock << 8
+			currentBlock = currentBlock | currentCharacter
+
+			# Bookkeeping
+			bitsProcessed += 8
+
+			if bitsProcessed % 512 == 0:
+				self.numBlocks += 1
+				self.parsedMessage[self.numBlocks] = currentBlock
+
+				bitsProcessed = 0
+				currentBlock = 0b0
+
+		# Checking if we ended at the end of a block
+		if bitsProcessed % 512 == 0:
+			self.numBlocks += 1
+			self.parsedMessage[self.numBlocks] = currentBlock
+
+			bitsProcessed = 0
+			currentBlock = 0b0
+
+		# Adding padding
+		if bitsProcessed >= 448:
+			return #FIXME
+			# probably needs to be an if else case as the padding for this
+			# and the "standard" case are rather different.
+
+		# Amount of padding needed to get to 448 bits in the last chunk
+		paddingLen = 448 - bitsProcessed
+
+		# paddingLen - 1 (for 1 bit) + 64 (for length at end)
+		padding = (0b1 << (paddingLen - 1 + 64)) | self.msgLen
+
+		# Total amount of shift
+		totalShift = paddingLen + 64
+
+		# Calculating the final block and adding it to the data structure
+		currentBlock = (currentBlock << totalShift) | padding
+		self.numBlocks += 1
+		self.parsedMessage[self.numBlocks] = currentBlock
+
+		# Breaking parsedMessage into 32-bit words
+		for i in range(1, self.numBlocks + 1):
+			block = str(hex(self.parsedMessage[i])).split('x')[1]
+			parsedBlock = []
+
+			for _ in range(16):
+				parsedBlock = [int(block[-8:], 16)] + parsedBlock
+				block = block[:-8]
+
+			self.parsedMessage[i] = parsedBlock
+
+	def output(self):
+		"""Outputs the results of the MD5 hash."""
+		outA = str(hex(self.A)).split('x')[1]
+		outB = str(hex(self.B)).split('x')[1]
+		outC = str(hex(self.C)).split('x')[1]
+		outD = str(hex(self.D)).split('x')[1]
+
+		output = outA + outB + outC + outD
+
+		# print(output)
+		# print(hex(self.A), hex(self.B), hex(self.C), hex(self.D))
+
+def main():
+	"""Main function if the program is executed from the command line."""
+	tmp = MD5("abc")
+
+if __name__ == "__main__":
+	main()
--- md5.py
+++ md5.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 """MD5 Implementation
 
-This is an implementation of the MD5 algorithm.
-This implementation is intended for academic use only.
+This is an implementation of the MD5 algorithm intended for academic use
+only. Implementation is based off of the existing Python2 MD5 module and 
+the psuedocode provide on Wikipedia. 
+
+RFC 1321 was also used as a reference.
 
 Authors:
 	Brandon Mendrick
@@ -11,190 +14,271 @@ Authors:
 """
 
 from math import floor, sin
+import sys
 
 class MD5:
-	"""MD5 class used to calculate MD5 hash values."""
-
-	# Number of 32 bit strings
-	bit32 = 2**32
-
-	# Number of 64 bit strings
-	bit64 = 2**64
-
-	# Amount of shift per round
-	shift = [
-		7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
-		5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
-		4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
-		6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21
-	]
-
-	# Constants adding during each round
-	constant = [None] * 64
-	for i in range(0, 64, 1):
-		constant[i] = floor(bit32 * abs(sin(i + 1)))
-
 	def __init__(self, message):
-		# Initial word values
-		self.A = 0x67452301
-		self.B = 0xefcdab89
-		self.C = 0x98badcfe
-		self.D = 0x10325476
+		## Constants
+		self.bit32 = 2**32
+		self.bit64 = 2**64
 
-		self.parse(message)
+		## Initial MD5 values
+		self._A = 0x67452301
+		self._B = 0xEFCDAB89
+		self._C = 0x98BADCFE
+		self._D = 0x10325476
 
-		self.calculate()
+		self.shift = []
+		self.shift += [7, 12, 17, 22] * 4
+		self.shift += [5,  9, 14, 20] * 4
+		self.shift += [4, 11, 16, 23] * 4
+		self.shift += [6, 10, 15, 21] * 4
+		
+		self.constant = [None] * 64
+		for i in range(0, 64, 1):
+			self.constant[i] = int(floor(self.bit32 * abs(sin(i + 1))))
 
-		self.output()
+		## Hashing given string
+		self.msg = message
+		if not self.msg:
+			self.msg = ""
 
-		return
+		self._hash()
 
-	def _rotate(self, target, num):
-		"""Left rotate function defined in MD5."""
-		return (target << num) | (target >> (32 - num))
+	## Public methods
 
-	def calculate(self):
-		"""Compute the A, B, C, D values for MD5."""
-		# For each 512-bit block
-		for j in range(1, self.numBlocks + 1):
-			currentBlock = self.parsedMessage[j]
+	def digest(self):
+		"""Returns a byte string of the hash value."""
+		parts = [self._resA, self._resB, self._resC, self._resD]
+		result = b''
 
-			currentA = self.A
-			currentB = self.B
-			currentC = self.C
-			currentD = self.D
-			function = None
-			position = None
+		for part in parts:
+			tmp = str(bin(part))[2::]
+			tmp = '0' * (32 - len(tmp)) + tmp
+			tmp = self._split(tmp, 8)[::-1]
 
+			for byte in tmp:
+				result += bytes([int(byte, 2)])
+
+		return result
+
+	def hexdigest(self):
+		"""Returns a hex string of the hash value."""
+		result = ""
+		for byte in self.digest():
+			result += "{:02x}".format(byte)
+
+		return result
+
+	def update(self, message):
+		"""Appends the input message to the current message.
+
+		After the appending, it updates the hash value for future calls
+		to (hex)digest.
+
+		"""
+		self.msg += message
+
+		self._hash()
+
+	## Private methods
+
+	def _addMod32(self, *args):
+		"""Adds all arguments modulo 2**32."""
+		result = 0
+		for arg in args:
+			result += arg
+
+		return (result % self.bit32)
+
+	def _createWords(self, chunk):
+		"""Breaks the input into 16 32-bit chunks.
+
+		input: 512-bit string
+		output: array[16] 32-bit numbers
+
+		Additionally, swaps order to be Little Endian.
+
+		"""
+		wordArray = [None] * 16
+		wordChunks = self._split(chunk, 32)
+		currentWord = 0
+
+		for wordChunk in wordChunks:
+			bytes_ = self._split(wordChunk, 8)
+			chunk = ""
+
+			# reversing the order of the bytes
+			for byte in bytes_:
+				chunk = byte + chunk
+
+			wordArray[currentWord] = int(chunk, 2)
+			currentWord += 1
+
+		return wordArray
+
+	def _hash(self):
+		"""Running the MD5 hashing algorithm."""
+		## Making life easier and more readable
+		A, B, C, D = self._A, self._B, self._C, self._D
+		F, G, H, I = self._F, self._G, self._H, self._I
+
+		# message to bitstring, padded, and split into 512-bit chunks
+		self.parsedMsg = self._split(self._pad(self._toBinString(self.msg)), 512)
+
+		for chunk in self.parsedMsg:
+			# Breaking the chunk into 16 32-bit words
+			words = self._createWords(chunk)
+
+			# bookkeeping
+			a, b, c, d = A, B, C, D
+			func, pos = None, None
+			
 			# Main loop
 			for i in range(64):
-				# Setting the function and position variables for this round
+				# Setting the func and pos variables for this round
 				if 0 <= i <= 15:
-					function = (currentB & currentC) | ((~currentB) & currentD)
-					position = i
+					func = F
+					pos = i
 
 				elif 16 <= i <= 31:
-					function = (currentD & currentB) | ((~currentD) & currentC)
-					position = ((5 * i) + 1) % 16
+					func = G
+					pos = ((5 * i) + 1) % 16
 
 				elif 32 <= i <= 47:
-					function = currentB ^ currentC ^ currentD
-					position = ((3 * i) + 5) % 16
+					func = H
+					pos = ((3 * i) + 5) % 16
 
 				elif 48 <= i <= 63:
-					function = currentC ^ (currentB | (~currentD))
-					position = (7 * i) % 16
+					func = I
+					pos = (7 * i) % 16
 
 				else:
 					print("Something went horribly wrong")
 					return
 
-				# Setting the A, B, C, D variables after the round
-				tempD = currentD
-				currentD = currentC
-				currentC = currentB
+				# Computing the value of B (only variable that is changed in a round)
+				tmpB = self._round(func, a, b, c, d, self.constant[i], words[pos], self.shift[i])
 
-				# Calculating B based on MD5 algorithm
-				rotateTarget = (currentA + function + MD5.constant[i] + currentBlock[position]) % MD5.bit32
-				currentB = (currentB + self._rotate(rotateTarget, MD5.shift[i])) % MD5.bit32
-				
-				currentA = tempD
+				# Moving A, B, C, D to their new homes
+				tmpD = d
+				d = c
+				c = b
+				b = tmpB
+				a = tmpD
 
-			# Adding the results of the round to the overall A, B, C, D variables
-			self.A = (self.A + currentA) % MD5.bit32
-			self.B = (self.B + currentB) % MD5.bit32
-			self.C = (self.C + currentC) % MD5.bit32
-			self.D = (self.D + currentD) % MD5.bit32
+			# Finishing processing of a chunk
+			A = self._addMod32(A, a)
+			B = self._addMod32(B, b)
+			C = self._addMod32(C, c)
+			D = self._addMod32(D, d)
 
-	def parse(self, message):
-		"""Parse message into 512-bit chunks and pad appropriately."""
-		# Message length in bits mod 2**64 for last 64-bits of padding
-		self.msgLen = (len(message.encode('utf-8')) * 8) % MD5.bit64
+		# assigning results to class variables for (hex)digest use
+		self._resA, self._resB, self._resC, self._resD = A, B, C, D
 
-		# Creating counter for number of blocks
-		self.numBlocks = 0
+	def _pad(self, bitString):
+		"""Pads the message according to MD5.
 
-		# Creating container for blocks
-		self.parsedMessage = {}
+		Pad the message with a 1 followed by 0s until the length is 
+		448 mod 512. Remaining 64 bits encode the length of the original
+		message.
 
-		# Creating array of integers from the message
-		msgInt = [ord(x) for x in message]
+		"""
+		msgLen = len(bitString)
 
-		# Counter for the number of bits processed
-		bitsProcessed = 0
+		## padding with zeros
+		bitString += '1'
+		while (len(bitString) % 512) != 448:
+			bitString += '0'
 
-		currentBlock = 0b0
-		while len(msgInt) > 0:		#FIXME should support empty messages. Not sure what happens....
-			currentCharacter = msgInt.pop(0)
+		## adding the 64 bit message length encoding
+		binLen = str(bin(msgLen))[2::]
 
-			# Appending the character to the current block
-			currentBlock = currentBlock << 8
-			currentBlock = currentBlock | currentCharacter
+		# making sure binLen is a byte length
+		while (len(binLen) % 8) != 0:
+			binLen = '0' + binLen
 
-			# Bookkeeping
-			bitsProcessed += 8
+		# checking if the length is over 64 bits
+		#TODO pretty sure this doesn't work...
+		if len(binLen) > 64:
+			bitString += '0' + '1' * 63
+			return bitString
 
-			if bitsProcessed % 512 == 0:
-				self.numBlocks += 1
-				self.parsedMessage[self.numBlocks] = currentBlock
+		# padding in this weird way to account for later shifts
+		padding = '0' * (64 - len(binLen))
+		padding += binLen
+		padding = self._split(padding, 32)
+		padding = ''.join(self._split(padding[1], 8)[::-1]) + ''.join(self._split(padding[0], 8)[::-1])
 
-				bitsProcessed = 0
-				currentBlock = 0b0
+		bitString += padding
 
-		# Checking if we ended at the end of a block
-		if bitsProcessed % 512 == 0:
-			self.numBlocks += 1
-			self.parsedMessage[self.numBlocks] = currentBlock
+		return bitString
 
-			bitsProcessed = 0
-			currentBlock = 0b0
+	def _split(self, bitString, size):
+		"""Return an array of the bitString broken into size chunks."""
+		return [ bitString[i:(i + size)] for i in range(0, len(bitString), size) ]
 
-		# Adding padding
-		if bitsProcessed >= 448:
-			return #FIXME
-			# probably needs to be an if else case as the padding for this
-			# and the "standard" case are rather different.
+	def _toBinString(self, message):
+		"""Convert string to bitstring."""
+		result = ""
+		for letter in message.encode("utf-8"):
+			result += "{:08b}".format(letter)
 
-		# Amount of padding needed to get to 448 bits in the last chunk
-		paddingLen = 448 - bitsProcessed
+		return result
 
-		# paddingLen - 1 (for 1 bit) + 64 (for length at end)
-		padding = (0b1 << (paddingLen - 1 + 64)) | self.msgLen
+	## MD5 functions
 
-		# Total amount of shift
-		totalShift = paddingLen + 64
+	def _F(self, x, y, z):
+		"""Compute (x and y) or ((not x) and z)."""
+		return (x & y) | ((~x) & z)
 
-		# Calculating the final block and adding it to the data structure
-		currentBlock = (currentBlock << totalShift) | padding
-		self.numBlocks += 1
-		self.parsedMessage[self.numBlocks] = currentBlock
+	def _G(self, x, y, z):
+		"""Compute (x and z) or (y and (not z))."""
+		return (x & z) | (y & (~z))
 
-		# Breaking parsedMessage into 32-bit words
-		for i in range(1, self.numBlocks + 1):
-			block = str(hex(self.parsedMessage[i])).split('x')[1]
-			parsedBlock = []
+	def _H(self, x, y, z):
+		"""Compute x xor y xor z."""
+		return x ^ y ^ z
 
-			for _ in range(16):
-				parsedBlock = [int(block[-8:], 16)] + parsedBlock
-				block = block[:-8]
+	def _I(self, x, y, z):
+		"""Compute y xor (x or (not z))."""
+		return y ^ (x | (~z))
 
-			self.parsedMessage[i] = parsedBlock
+	def _round(self, func, a, b, c, d, constant, chunk, shift):
+		"""Compute a round of the MD5 algorithm."""
+		result = self._addMod32(a, func(b, c, d), constant, chunk)
+		result = self._rotateLeft(result, shift)
+		result = result % self.bit32
+		result = self._addMod32(result, b)
 
-	def output(self):
-		"""Outputs the results of the MD5 hash."""
-		outA = str(hex(self.A)).split('x')[1]
-		outB = str(hex(self.B)).split('x')[1]
-		outC = str(hex(self.C)).split('x')[1]
-		outD = str(hex(self.D)).split('x')[1]
+		return result
 
-		output = outA + outB + outC + outD
+	def _rotateLeft(self, bitString, shift):
+		"""Compute the left rotation of the bitString shift places."""
+		return (bitString << shift) | (bitString >> (32 - shift))
 
-		print(output)
-		print(hex(self.A), hex(self.B), hex(self.C), hex(self.D))
+## Public module interface
+def new(args=None):
+	"""Create new instance of MD5 class."""
+	return MD5(args)
+
+def md5(args=None):
+	"""Create new instance of MD5 class."""
+	return MD5(args)
 
 def main():
-	"""Main function if the program is executed from the command line."""
-	tmp = MD5("a")
+	"""Main function for testing purposes."""
+	# obj = md5("12345678901234567890123456789012345678901234567890123456789012345678901234567890")
+	obj = md5("abc")
 
+	# string = ""
+	# for i in range(1000):
+	# 	string += "1234567890"
+	# string += "123"
+	# obj = md5(string)
+
+	print(obj.hexdigest())
+
+"""Calling main for testing purposes."""
 if __name__ == "__main__":
 	main()
